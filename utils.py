df978868ceabfa0d3ec8f53a3bf0429ce70e0b2b
Sumit Kumar Jethani Jethani <55793026+sumitkumarjethani@users.noreply.github.com>
2023-03-05 19:16:27 +0100

    train_size param in cv

--- utils.py
+++ utils.py
@@ -12,5 +12,6 @@ def get_train_test_data():
     X_test , y_test = test.drop(columns=['stroke']), test.stroke
     return (X_train, X_test, y_train, y_test)
 
-def get_stratified_shuffle_cv(n_splits=5, test_size=0.2):
-    return StratifiedShuffleSplit(n_splits=n_splits, test_size=test_size, random_state=seed)
+def get_stratified_shuffle_cv(n_splits=5, test_size=0.2, train_size=0.8):
+    return StratifiedShuffleSplit(n_splits=n_splits, test_size=test_size,
+                                  train_size=train_size, random_state=seed)
