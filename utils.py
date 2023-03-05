f05ad1f092d72f149a612983cd9ef5662a2e962f
Sumit Kumar Jethani Jethani <55793026+sumitkumarjethani@users.noreply.github.com>
2023-03-05 20:10:56 +0100

    model evaluation functions

--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
 # Common functions for training and evaluating models
 
 import pandas as pd
-from sklearn.model_selection import StratifiedShuffleSplit
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.metrics import confusion_matrix, classification_report, roc_curve, roc_auc_score
+
+# Datasets functions
 def get_train_test_data():
     train = pd.read_csv('/kaggle/input/oversampling-smote/train_oversampled.csv')
     test = pd.read_csv('/kaggle/input/oversampling-smote/test.csv')
@@ -13,3 +18,54 @@ def get_train_test_data():
 def get_stratified_shuffle_cv(n_splits=5, test_size=0.2, train_size=0.8, seed=99):
     return StratifiedShuffleSplit(n_splits=n_splits, test_size=test_size,
                                   train_size=train_size, random_state=seed)
+
+# Model evaluation functions
+def get_confusion_matrix(y_test, y_pred):
+    return confusion_matrix(y_true=y_test, y_pred=y_pred)
+
+def print_metrics(y_test, y_pred, model_name):
+    
+    print(classification_report(y_test, y_pred))
+    
+    fpr, tpr, tr = roc_curve(y_test, y_pred)
+    auc = roc_auc_score(y_test, y_pred)
+    plt.plot(fpr,tpr,'b',label="AUC="+str(auc))
+    plt.plot([0,1],[0,1],'k--')
+    plt.title(model_name)
+    plt.grid()
+    plt.legend(loc='best')
+    plt.show()
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.grid(False)
