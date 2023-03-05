dcd9dc9acb688554bb31e013f25a01d4f8ea154d
Sumit Kumar Jethani Jethani <55793026+sumitkumarjethani@users.noreply.github.com>
2023-03-05 19:23:05 +0100

    remove default seed

--- utils.py
+++ utils.py
@@ -3,8 +3,6 @@
 import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit
 
-seed = 99
-
 def get_train_test_data():
     train = pd.read_csv('/kaggle/input/oversampling-smote/train_oversampled.csv')
     test = pd.read_csv('/kaggle/input/oversampling-smote/test.csv')
@@ -12,6 +10,6 @@ def get_train_test_data():
     X_test , y_test = test.drop(columns=['stroke']), test.stroke
     return (X_train, X_test, y_train, y_test)
 
-def get_stratified_shuffle_cv(n_splits=5, test_size=0.2, train_size=0.8):
+def get_stratified_shuffle_cv(n_splits=5, test_size=0.2, train_size=0.8, seed=99):
     return StratifiedShuffleSplit(n_splits=n_splits, test_size=test_size,
                                   train_size=train_size, random_state=seed)
